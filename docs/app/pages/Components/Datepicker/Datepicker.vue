0afe8807ed1383ab7125c6e0e060e80fe4099345
Muhammed Shafi <shafimsp@gmail.com>
2018-02-22 09:02:29 +0530

    docs(MdDatepicker) Datepicker wrapper

--- docs/app/pages/Components/Datepicker/Datepicker.vue
+++ docs/app/pages/Components/Datepicker/Datepicker.vue
@@ -1,4 +1,5 @@
 <example src="./examples/BasicDatepicker.vue" />
+<example src="./examples/DatepickerWrapper.vue" />
 <example src="./examples/LabeledDatepicker.vue" />
 <example src="./examples/CancelOpenDatepicker.vue" />
 <example src="./examples/DisabledDatesDatepicker.vue" />
@@ -13,6 +14,8 @@
       <h2>Datepicker</h2>
 
       <code-example title="Basic Example" :component="examples['basic-datepicker']" />
+
+      <code-example title="Datepicker Wrapper" :component="examples['datepicker-wrapper']" />
 
       <code-example title="Labeled Example" :component="examples['labeled-datepicker']" />
     </div>
---
+++ docs/app/pages/Components/Datepicker/examples/DatepickerWrapper.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <md-datepicker v-model="selectedDate" >
+      <md-button md-datepicker-trigger class="md-icon-button">
+        <md-icon>event</md-icon>
+      </md-button>
+      <label>{{ displayDate }}</label>
+    </md-datepicker>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'DatepickerWrapper',
+    data: () => ({
+      selectedDate: null
+    }),
+    computed: {
+      displayDate ()  {
+        if(this.selectedDate) {
+          return this.selectedDate.toDateString()
+        }
+        return 'Choose Date'
+      }
+    }
+  }
+</script>
+
+<style lang="scss">
+  .md-datepicker {
+    margin-top: 30px;
+
+    label {
+      line-height: 40px;
+    }
+  }
+</style>
